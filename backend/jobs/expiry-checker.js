4b9957b8df8a74f7a406e803e8f308f53164ef25
sushantjagtap5543 <sushantjagtap.naandi@gmail.com>
2026-03-01 08:04:43 +0530

    Delete backend directory

--- backend/jobs/expiry-checker.js
+++
@@ -1,7 +0,0 @@
-const BillingService = require('../services/billing.service');
-const pool = new require('pg').Pool({ connectionString: process.env.DATABASE_URL });
-
-setInterval(async () => {
-  const service = new BillingService(pool);
-  await service.checkExpiry();
-}, 3600000); // Hourly
