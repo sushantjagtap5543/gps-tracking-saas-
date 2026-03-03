8cb0f9b3bc1ae52bc8b8033fa05209d13eaa8455
sushantjagtap5543 <sushantjagtap.naandi@gmail.com>
2026-03-03 17:53:36 +0530

    Create Vehicles.js

---
+++ frontend/src/pages/Vehicles.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+export default function Vehicles() {
+  return (
+    <>
+      <Navbar />
+      <div style={{ padding: 20 }}>
+        <h2>Vehicles Management</h2>
+        <p>Vehicle CRUD UI here</p>
+      </div>
+    </>
+  );
+}
